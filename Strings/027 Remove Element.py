0138bd1647d23a58b73a932678ca261b84e75611
Ola320 <ola09011@gmail.com>
2025-04-15 15:20:32 +0200

    used two ways of solving the one and two pointers algoythms

---
+++ Strings/027 Remove Element.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+
+        i = 0
+        n = len(nums)
+        while i <= n:
+            if nums[i] == val:
+                nums[i] = nums[n-1]
+                n -= 1
+
+            else:
+                i += 1
+
+        return n
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if val != nums[i]:
+                nums[k] = nums[i]
+                k += 1
+
+            return k
+
+
